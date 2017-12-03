ce444c44090f1da27681ac659f28ac52aa52b4c1
CMahon92 <carleenmahon92@gmail.com>
2017-12-03 17:24:00 -0600

    alerts, prompts, console.log

---
+++ jsIntro.js
@@ -0,0 +1,9 @@
+//These are the following pop ups 
+
+var firstName = prompt("What is your first name?");
+var lastName = prompt("What is your last name?");
+var age = prompt("How old are you?");
+
+alert("It'\s a pleasure to meet you " + firstName +" "+ lastName);
+console.log ("Hey! I'\m console.log! It's also nice to meet you " + firstName + " " + lastName );
+console.log ("You are " +age+ " years old");
